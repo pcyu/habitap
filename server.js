3989abd2e7675865b8e70c28a284d3d474e431ae
Peter Yu <PeterYu@gmail.com>
2017-09-21 13:29:39 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+
+
 const express = require('express');
 const {PORT, DATABASE_URL} = require('./config');
-const {Habit} = require('./models');
 const app = express();
-
+app.use(morgan('common'));
 app.use(bodyParser.json());
+
 app.use( '/', express.static(__dirname + '/public') );
 app.use( '/node_modules', express.static(__dirname + '/node_modules') );
 app.use( '/src', express.static(__dirname + '/src') );
+
+const {Person} = require('./models');
 
 app.get('/heartbeat', function(req, res) {
   res.json({
