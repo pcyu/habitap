905f5ffbbc6150227cee7db4466d19e06cc0c948
Peter Yu <PeterYu@gmail.com>
2017-09-20 22:02:26 -0400

    updated personRouter.js

---
+++ personRouter.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const {Person} = require('./models');
+
+router.use(jsonParser);
+
+router.get('/', (req, res) => {
+    Person
+      .find()
+      .exec()
+      .then(persons => {
+        res.json({
+          persons: persons.map( person => person.apiRepr() )
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        return res.status(500).json({message: 'Internal server error'});
+      });
+  });
+
+module.exports = router;
+  
