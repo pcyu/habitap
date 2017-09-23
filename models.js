e9bede1ffb975e4a76ae1d943031705844bc2e4c
Peter Yu <PeterYu@gmail.com>
2017-09-23 16:58:05 -0400

    output json file through get request via postman

--- models.js
+++ models.js
@@ -24,12 +24,23 @@ const mongoose = require('mongoose');
 const personSchema = mongoose.Schema({
   id: String,
   name: String,
+  habits: [
+    {
+      id: String,
+      title: String,
+      isactive: Boolean,
+      startDate: String,
+      endDate: String,
+      publishedAt: Number,
+    }
+  ]
 });
 
 personSchema.methods.apiRepr = function() {
   return {
     id: this._id,
     name: this.name,
+    habits: this.habits
   };
 }
 
