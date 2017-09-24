062e5fee85498ee9ed0c6e9aed82c28189053851
Peter Yu <PeterYu@gmail.com>
2017-09-24 12:51:35 -0400

    updated mock.json

--- models.js
+++ models.js
@@ -1,24 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const personSchema = mongoose.Schema({
-//   name: {type: String},  // e.g. account, personal, or tax
-//   active: {type: Boolean},
-//   age: {type: Number}
-// });
-
-// personSchema.methods.apiRepr = function() {
-//   return {
-//     id: this._id,
-//     name: this.name,
-//     active: this.active,
-//     age: this.age
-//   };
-// }
-
-// const Person = mongoose.model('Persons', personSchema);
-
-// module.exports = {Person};
-
 const mongoose = require('mongoose');
 
 const personSchema = mongoose.Schema({
--- personRouter.js
+++ personRouter.js
@@ -4,26 +4,26 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const {Person} = require('./models');
 
-// router.delete('/:id', (req, res) => {
-//   Person
-//     .findByIdAndRemove(req.params.id, function(err, value) {
-//       if(err) {
-//         const message = `It appears that the document with id (${req.params.id}) does not exist.`;
-//         console.error(message);
-//         return res.status(400).send(message);
-//       }
-//     })
-//     .exec()
-//     .then( person => {
-//       const message = `204 / The document with id ${req.params.id} has been deleted`;
-//       console.log(message);
-//       return res.json(message).end();
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       return res.status(500).json({message: 'Internal server error'});
-//     });
-// });
+router.delete('/:id', (req, res) => {
+  Person
+    .findByIdAndRemove(req.params.id, function(err, value) {
+      if(err) {
+        const message = `It appears that the document with id (${req.params.id}) does not exist.`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    })
+    .exec()
+    .then( person => {
+      const message = `204 / The document with id ${req.params.id} has been deleted`;
+      console.log(message);
+      return res.json(message).end();
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(500).json({message: 'Internal server error'});
+    });
+});
 
 router.get('/', (req, res) => {
   Person
@@ -40,68 +40,69 @@ router.get('/', (req, res) => {
     });
 });
 
-// router.get('/:id', (req, res) => {
-//   Person
-//     .findById(req.params.id)
-//     .exec()
-//     .then( person => {
-//       res.json({
-//         persons: person.apiRepr()
-//       })
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       return res.status(500).json({message: 'Internal server error'});
-//     });
-// });
+router.get('/:id', (req, res) => {
+  Person
+    .findById(req.params.id)
+    .exec()
+    .then( person => {
+      res.json({
+        persons: person.apiRepr()
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(500).json({message: 'Internal server error'});
+    });
+});
 
-// router.post('/', (req, res) => {
-//   const requiredFields = ['dIsType', 'longName', 'shortName'];
-//   for(let i = 0; i < requiredFields.length; i++) {
-//     const field = requiredFields[i];
-//     if(!(field in req.body)) {
-//       const message = `The value for \`${field}\` is missing.`
-//       console.error(message);
-//       return res.status(400).send(message);
-//     }
-//   }
-//   Person
-//     .create({
-//       dIsType: req.body.dIsType,
-//       shortName: req.body.shortName,
-//       longName: req.body.longName,
-//       moreInfo: req.body.moreInfo
-//     })
-//     .then(
-//       person => res.status(201).json(person.apiRepr())
-//     )
-//     .catch(err => {
-//       console.error(err);
-//       return res.status(500).json({message: 'Internal server error'});
-//     });
-// });
+router.post('/', (req, res) => {
+  const requiredFields = ['name', 'habits'];
+  for(let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if(!(field in req.body)) {
+      const message = `The value for \`${field}\` is missing.`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  Person
+    .create({
+      name: req.body.name,
+      habits: req.body.habits,
+    })
+    .then(
+      person => res.status(201).json(person.apiRepr())
+    )
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({message: 'Internal server error'});
+    });
+});
 
-// router.put('/:id', (req, res) => {  //p002
-//   if(req.params.id !== req.body.id) {
-//     const message = `The request path (${req.params.id}) and the request body id (${req.body.id}) must match.`;
-//     console.error(message);
-//     return res.status(400).json({message: message});
-//   }
-//   const toUpdate = {};
-//   const updateableFields = ['name', 'active', 'age'];
-//   updateableFields.forEach(field => {
-//     if(field in req.body) {
-//       toUpdate[field] = req.body[field];
-//     }
-//   });
-//   Person
-//     .findByIdAndUpdate(req.params.id, {$set: toUpdate})
-//     .exec()
-//     .then( person => res.json(204).end() )
-//     .catch(err => {
-//       console.error(err);
-//       return res.status(500).json({message: 'Internal server error'})
-//     });
-// });
+router.put('/:id', jsonParser, (req, res) => {  //p002
+  const requiredFields = ['name', 'habits', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating habits \`${req.params.id}\``);
+
+  Person.update({
+    name: req.body.name,
+    habits: req.body.habits,
+    id: req.params.id
+  });
+
+  res.status(204).end();
+});
 
 module.exports = router;
