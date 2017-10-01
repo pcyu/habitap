76e17f554f3c7832e017993fa9e0b712cf029258
Peter Yu <PeterYu@gmail.com>
2017-10-01 17:06:02 -0400

    removed mock.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 mongoose.Promise = global.Promise;
 
