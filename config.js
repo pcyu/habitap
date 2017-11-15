7fbe2665c41021278d87539f477e1a56702709d6
Peter Yu <PeterYu@gmail.com>
2017-11-15 18:47:04 -0500

    changed server.js port number to troubleshoot code with another version

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ global.DATABASE_URL ||
 'mongodb://localhost/habitap';
 // "mongodb://admin:admin@ds163294.mlab.com:63294/habitap";
 
-exports.PORT = process.env.PORT || 3007;
+exports.PORT = process.env.PORT || 3008;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
