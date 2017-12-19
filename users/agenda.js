915dc23d60eaee81fd8eab924fa3deca48bcd51f
Peter Yu <PeterYu@gmail.com>
2017-12-18 21:27:29 -0500

    check agenda.js within users directory to find the todayAnswer job

--- users/agenda.js
+++ users/agenda.js
@@ -5,10 +5,10 @@ const {DATABASE_URL} = require('../config');
 var agenda = new Agenda({db: { address: DATABASE_URL, collection: 'users' }});
 
 agenda.define('reset dailyCheck', function(job, done) {
-  console.log("daily check reset!")
-  User.update({}, {$set: {firstName: "huehue"}}, {multi: true}, done);
+  console.log("daily check reset!!!")
+  User.update({}, {$set: {firstName: "peter"}}, {multi: true}, done);
 
-  // User.update({}, {$set: {"habits.$[].todayAnswer":  false}}, {multi: true}, done);
+  // User.find({"habits": {$exists: true}}, {$set: {"habits.todayAnswer": false}}, {multi: true}, done);
 
 });
 
@@ -16,3 +16,13 @@ agenda.on('ready', function(){
   agenda.every('1 minute', 'reset dailyCheck');
   agenda.start();
 });
+
+// {username: req.params.username, "habits.habitId": req.params.habitId},
+// {
+//   $push: {
+//     "habits.$.dailyCheck": req.body.habit
+//   },
+//   $set: {
+//     "habits.$.todayAnswer": true
+//   }
+// }
