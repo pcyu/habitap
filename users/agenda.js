166099fde6d9334e8d70f91e142f38d7710e60d0
Peter Yu <PeterYu@gmail.com>
2018-01-02 12:10:52 -0500

    mongo 3.6 push does not work, but documenation suggests that 3.4 allows it to work

--- users/agenda.js
+++ users/agenda.js
@@ -5,8 +5,47 @@ const moment = require('moment');
 
 var agenda = new Agenda({db: { address: DATABASE_URL, collection: 'agenda' }});
 
+
+	// User.update(
+  //   {username: req.params.username, "habits.habitId": req.params.habitId},
+  //   {
+	// 		$push: {
+	// 			"habits.$.dailyCheck": req.body.habit
+	// 		},
+	// 		$set: {
+	// 			"habits.$.todayAnswer": true
+	// 		}
+  //   }
+	// )
+	// .then((results) => {
+	// 	User.findOne({username: req.params.username}).then(user => {
+	// 			for (var habit of user.habits) {
+	// 				habit.active = (habit.dailyCheck.length < 15);
+	// 			}
+	// 			user.save();
+	// 		})
+	// })
+
+
+agenda.define('catch missed days', function(job, done) {
+
+  User.find()
+  .then(users => {
+    for (var user of users) {
+      for (var habit of user.habits) {
+        if (habit.todayAnswer===false && habit.active===true) {
+        console.log(moment().format('LLL'), habit.dailyCheck)
+          habit.dailyCheck.push(2);
+        }
+      }
+    user.save();
+    }
+  });
+  done();
+});
+
 agenda.define('reset dailyCheck', function(job, done) {
-  console.log(moment().format('LLL'));
+  console.log(moment().format('LLL'), "reset dailyCheck");
 
   User.find()
 		.then(users => {
@@ -23,6 +62,7 @@ agenda.define('reset dailyCheck', function(job, done) {
 // (min hour dayofmonth month dayofweek)
 
 agenda.on('ready', function(){
+  agenda.every('10 seconds' , 'catch missed days');
   agenda.every('46 21 * * *' , 'reset dailyCheck');
   agenda.start();
 });
