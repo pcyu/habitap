c6d9e553d02bfcfdc47d59cbd01053b04037faf0
Peter Yu <PeterYu@gmail.com>
2018-01-03 12:25:42 -0500

    removed config.example.js

--- configexample.js
+++
@@ -1,9 +0,0 @@
-// exports.DATABASE_URL = process.env.DATABASE_URL ||
-// global.DATABASE_URL ||
-// // 'mongodb://localhost/habitap';
-// "mongodb://username:password@ds919191.mlab.com:13215/habitap";
-// //dummy acc info for mlab
-
-// exports.PORT = process.env.PORT || 8080;
-// exports.JWT_SECRET = process.env.JWT_SECRET;
-// exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
