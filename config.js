9b2d4f06abdedf884d629224c356c848b1f29177
Peter Yu <PeterYu@gmail.com>
2018-01-03 12:17:07 -0500

    updated gitignore file and config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+global.DATABASE_URL ||
+// 'mongodb://localhost/habitap';
+"mongodb://admin:admin@ds163294.mlab.com:63294/habitap";
+
+exports.PORT = process.env.PORT || 3007;
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
