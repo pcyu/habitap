b5fbe8d4455de8af2bc5949b5db838b38c46a949
Peter Yu <PeterYu@gmail.com>
2018-01-03 12:22:46 -0500

    fixing database url issue, tryna troubleshoot

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
 global.DATABASE_URL ||
-// 'mongodb://localhost/habitap';
-"mongodb://admin:admin@ds163294.mlab.com:63294/habitap";
+'mongodb://localhost/habitap';
 
 exports.PORT = process.env.PORT || 3007;
 exports.JWT_SECRET = process.env.JWT_SECRET;
