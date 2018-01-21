42ff01b664d44d443b28c1e51d88254ffe59f226
Peter Yu <PeterYu@gmail.com>
2018-01-21 14:44:44 -0500

    removed jquery.js

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -1,20 +1,20 @@
-(($) => {
+// (($) => {
 
-  var app = {
-    init: function() {
-      app.activateEdit();
-    },
-    activateEdit: function() {
-      for (let i = 0; i < 5; i++) {
-    	$("#edit-button-"+i).on('click', function(event) {
-        $("#text-box-"+i).css("display", "block")
-      });
-      }
-    }
-  };
+//   var app = {
+//     init: function() {
+//       app.activateEdit();
+//     },
+//     activateEdit: function() {
+//       for (let i = 0; i < 5; i++) {
+//     	$("#edit-button-"+i).on('click', function(event) {
+//         $("#text-box-"+i).css("display", "block")
+//       });
+//       }
+//     }
+//   };
 
-  $(window).on('load', () => {
-    app.init();  //c028
-  });
+//   $(window).on('load', () => {
+//     app.init();  //c028
+//   });
 
-})(window.jQuery);
+// })(window.jQuery);
