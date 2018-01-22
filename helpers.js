709e68ca1a5d88b99f675a5ef84ac535fbedf006
Peter Yu <PeterYu@gmail.com>
2018-01-22 14:43:57 -0500

    added new nav bar functionality

--- helpers.js
+++ helpers.js
@@ -2,6 +2,7 @@ exports.navs = [
   {name: 'Home', path: '/', type: ['public']},
   {name: 'Register', path: '/auth/register', type: ['public']},
   {name: 'Login', path: '/auth/login', type: ['public']},
+  {name: 'Demo', path: '/auth/demo', type: ['public']},
   {name: 'Dashboard', path: '/users/dashboard', type: ['private']},
   {name: 'New Habit', path: '/users/new', type: ['private']},
   {name: 'History', path: '/users/history', type: ['private']},
