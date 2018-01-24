c80de7d5e36f3f3e46e1fcc180a93fc9d58952af
Peter Yu <PeterYu@gmail.com>
2018-01-24 13:13:06 -0500

    migrated code to auth instead of user, check for bugs

--- auth/router.js
+++ auth/router.js
@@ -1,9 +1,12 @@
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const passport = require('passport');
+const jsonParser = bodyParser.json();
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 const app = express();
+const {User} = require('../users/model');
 app.use(cookieParser());
 
 const createAuthToken = user => {
@@ -30,6 +33,10 @@ router.get('/register', (req, res) => {
   res.render('register');
 });
 
+router.get('/success', (req, res) => {
+  res.render('success');
+})
+
 router.get('/failure', (req, res) => {
   res.render('failure');
 })
@@ -46,6 +53,110 @@ router.get('/logout', (req, res) => {
 //  ===========================================================================
 //                                      POST
 //  ===========================================================================
+router.post('/register', jsonParser, (req, res) => {
+	const requiredFields = ['username', 'password'];
+	const missingField = requiredFields.find(field => !(field in req.body));
+
+	if (missingField) {
+			return res.status(422).json({
+					code: 422,
+					reason: 'ValidationError',
+					message: 'Missing field',
+					location: missingField
+			});
+	}
+
+	const stringFields = ['username', 'password'];
+	const nonStringField = stringFields.find(
+			field => field in req.body && typeof req.body[field] !== 'string'
+	);
+
+	if (nonStringField) {
+			return res.status(422).json({
+					code: 422,
+					reason: 'ValidationError',
+					message: 'Incorrect field type: expected string',
+					location: nonStringField
+			});
+	}
+
+	const explicityTrimmedFields = ['username', 'password'];  //c030
+	const nonTrimmedField = explicityTrimmedFields.find(
+			field => req.body[field].trim() !== req.body[field]
+	);
+
+	if (nonTrimmedField) {
+			return res.status(422).json({
+					code: 422,
+					reason: 'ValidationError',
+					message: 'Cannot start or end with whitespace',
+					location: nonTrimmedField
+			});
+	}
+
+	const sizedFields = {
+			username: {
+					min: 1
+			},
+			password: {
+					min: 10,
+					max: 72  //c031
+			}
+	};
+	const tooSmallField = Object.keys(sizedFields).find(
+			field =>
+					'min' in sizedFields[field] &&
+					req.body[field].trim().length < sizedFields[field].min
+	);
+	const tooLargeField = Object.keys(sizedFields).find(
+			field =>
+					'max' in sizedFields[field] &&
+					req.body[field].trim().length > sizedFields[field].max
+	);
+
+	if (tooSmallField || tooLargeField) {
+		return res.status(422).json({
+			code: 422,
+			reason: 'ValidationError',
+			message: tooSmallField
+				? `Must be at least ${sizedFields[tooSmallField]
+					.min} characters long`
+				: `Must be at most ${sizedFields[tooLargeField]
+					.max} characters long`,
+			location: tooSmallField || tooLargeField
+		});
+	}
+
+	let {username, password} = req.body;  //c032
+
+	return User.find({username})
+		.count()
+		.then(count => {
+			if (count > 0) {
+				return Promise.reject({  //c033
+					code: 422,
+					reason: 'ValidationError',
+					message: 'Username already taken',
+					location: 'username'
+				});
+			}
+			return User.hashPassword(password);  //c034
+		})
+		.then(hash => {
+			return User.create({
+				username,
+				password: hash
+			});
+		})
+		.then(user => {res.redirect(`/success`)})
+		.catch(err => {  //c035
+				if (err.reason === 'ValidationError') {
+					return res.status(err.code).json(err);
+				}
+				res.status(500).json({code: 500, message: 'Internal server error'});
+		});
+});
+
 router.post(
   '/login',
   // The user provides a username and password to login
--- users/router.js
+++ users/router.js
@@ -1,9 +1,7 @@
 // users router
 require('dotenv').config();
-const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const jsonParser = bodyParser.json();
 const jwt = require('jsonwebtoken');
 const moment = require('moment');
 const passport = require('passport');
@@ -237,110 +235,6 @@ router.get('/habitstart', passport.authenticate('jwt', {session: false}), (req, 
 //  ===========================================================================
 //                                      POST
 //  ===========================================================================
-router.post('/register', jsonParser, (req, res) => {
-	const requiredFields = ['username', 'password'];
-	const missingField = requiredFields.find(field => !(field in req.body));
-
-	if (missingField) {
-			return res.status(422).json({
-					code: 422,
-					reason: 'ValidationError',
-					message: 'Missing field',
-					location: missingField
-			});
-	}
-
-	const stringFields = ['username', 'password'];
-	const nonStringField = stringFields.find(
-			field => field in req.body && typeof req.body[field] !== 'string'
-	);
-
-	if (nonStringField) {
-			return res.status(422).json({
-					code: 422,
-					reason: 'ValidationError',
-					message: 'Incorrect field type: expected string',
-					location: nonStringField
-			});
-	}
-
-	const explicityTrimmedFields = ['username', 'password'];  //c030
-	const nonTrimmedField = explicityTrimmedFields.find(
-			field => req.body[field].trim() !== req.body[field]
-	);
-
-	if (nonTrimmedField) {
-			return res.status(422).json({
-					code: 422,
-					reason: 'ValidationError',
-					message: 'Cannot start or end with whitespace',
-					location: nonTrimmedField
-			});
-	}
-
-	const sizedFields = {
-			username: {
-					min: 1
-			},
-			password: {
-					min: 10,
-					max: 72  //c031
-			}
-	};
-	const tooSmallField = Object.keys(sizedFields).find(
-			field =>
-					'min' in sizedFields[field] &&
-					req.body[field].trim().length < sizedFields[field].min
-	);
-	const tooLargeField = Object.keys(sizedFields).find(
-			field =>
-					'max' in sizedFields[field] &&
-					req.body[field].trim().length > sizedFields[field].max
-	);
-
-	if (tooSmallField || tooLargeField) {
-		return res.status(422).json({
-			code: 422,
-			reason: 'ValidationError',
-			message: tooSmallField
-				? `Must be at least ${sizedFields[tooSmallField]
-					.min} characters long`
-				: `Must be at most ${sizedFields[tooLargeField]
-					.max} characters long`,
-			location: tooSmallField || tooLargeField
-		});
-	}
-
-	let {username, password} = req.body;  //c032
-
-	return User.find({username})
-		.count()
-		.then(count => {
-			if (count > 0) {
-				return Promise.reject({  //c033
-					code: 422,
-					reason: 'ValidationError',
-					message: 'Username already taken',
-					location: 'username'
-				});
-			}
-			return User.hashPassword(password);  //c034
-		})
-		.then(hash => {
-			return User.create({
-				username,
-				password: hash
-			});
-		})
-		.then(user => {res.redirect(`/auth/login`)})
-		.catch(err => {  //c035
-				if (err.reason === 'ValidationError') {
-					return res.status(err.code).json(err);
-				}
-				res.status(500).json({code: 500, message: 'Internal server error'});
-		});
-});
-
 router.post('/new', passport.authenticate('jwt', {session: false}), (req, res) => {
 	User.find({"username": req.user})
 	.then(user=>{
